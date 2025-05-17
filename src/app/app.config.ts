f986a4a2ec4327e755153b532efead5ccd888bb4
Nikita Gaziev <zobweyt@gmail.com>
2025-05-18 00:56:30 +0300

    style: add eslint and prettier (#6)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,16 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
-} from '@angular/core';
+} from "@angular/core";
 import {
   provideClientHydration,
   withEventReplay,
-} from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { provideEventPlugins } from '@taiga-ui/event-plugins';
-import { routes } from './app.routes';
+} from "@angular/platform-browser";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideRouter } from "@angular/router";
+import { provideEventPlugins } from "@taiga-ui/event-plugins";
+
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
