ea9d407004519959f03a1c872b40303c3795440e
Nikita Gaziev <zobweyt@gmail.com>
2025-05-18 03:46:31 +0300

    feat: add header widget

---
+++ src/widgets/header/ui/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+import { AppHeaderNavigationComponent } from "../navigation/navigation.component";
+
+@Component({
+  selector: "app-header",
+  templateUrl: "header.component.html",
+  styleUrl: "header.component.scss",
+  imports: [AppHeaderNavigationComponent, RouterLink],
+})
+export class AppHeaderComponent {}
---
+++ src/widgets/header/ui/navigation/navigation.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { TuiButton, TuiDataList, TuiDropdown } from "@taiga-ui/core";
+
+import { NAVIGATION_ITEMS } from "../../config/navigation.config";
+
+@Component({
+  selector: "app-header-navigation",
+  templateUrl: "navigation.component.html",
+  styleUrl: "navigation.component.scss",
+  imports: [RouterLink, RouterLinkActive, TuiButton, TuiDataList, TuiDropdown],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppHeaderNavigationComponent {
+  protected readonly items = NAVIGATION_ITEMS;
+  protected readonly dropdownOpenStates: { [routerLink: string]: boolean } = {};
+}
