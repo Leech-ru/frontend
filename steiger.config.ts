d4c6a7c1179c45f235f807cba2d2934f84cb98ad
Sova <108209320+ncesova@users.noreply.github.com>
2025-05-18 11:32:10 +0300

    style: add steiger

---
+++ steiger.config.ts
@@ -0,0 +1,20 @@
+import fsd from "@feature-sliced/steiger-plugin";
+import { defineConfig } from "steiger";
+
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    // disable the `public-api` rule for files in the Shared layer
+    files: ["./src/shared/**"],
+    rules: {
+      "fsd/public-api": "off",
+    },
+  },
+  {
+    files: ["./src/app/@x/**"],
+    rules: {
+      "fsd/no-public-api-sidestep": "off",
+      "fsd/forbidden-imports": "off",
+    },
+  },
+]);
