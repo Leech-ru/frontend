ab31a513b0953aad709e8bffd90e44823cd11fc1
Nikita Gaziev <zobweyt@gmail.com>
2025-05-19 18:16:20 +0300

    refactor(footer): remove unused imports

--- src/widgets/footer/ui/footer.component.ts
+++ src/widgets/footer/ui/footer.component.ts
@@ -1,24 +1,13 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { TuiActiveZone } from "@taiga-ui/cdk/directives/active-zone";
-import { TuiObscured } from "@taiga-ui/cdk/directives/obscured";
-import { TuiButton, TuiDropdown, TuiLink } from "@taiga-ui/core";
-import { TuiChevron } from "@taiga-ui/kit";
+import { TuiDropdown, TuiLink } from "@taiga-ui/core";
 
 import { FOOTER_SECTIONS } from "../config/footer.config";
 
 @Component({
   standalone: true,
   selector: "app-footer",
-  imports: [
-    TuiActiveZone,
-    TuiButton,
-    TuiChevron,
-    TuiDropdown,
-    TuiObscured,
-    TuiLink,
-    RouterLink,
-  ],
+  imports: [RouterLink, TuiDropdown, TuiLink],
   templateUrl: "footer.component.html",
   styleUrl: "footer.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
