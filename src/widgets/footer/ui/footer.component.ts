42e80a70925207ec14ec14df62e913f189fd54b2
Ramir Vorobyev <rrramirrr@yandex.ru>
2025-05-19 15:34:58 +0300

    feat: :ambulance: footer update
    Сделал верхнюю часть футера, но не со всеми стилями. Требуется поставить стили на текст и дописать конфиг для верхней части футера
    
    Стилизация футера

--- src/widgets/footer/ui/footer.component.ts
+++ src/widgets/footer/ui/footer.component.ts
@@ -1,15 +1,28 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 import { TuiActiveZone } from "@taiga-ui/cdk/directives/active-zone";
 import { TuiObscured } from "@taiga-ui/cdk/directives/obscured";
-import { TuiButton, TuiDropdown } from "@taiga-ui/core";
+import { TuiButton, TuiDropdown, TuiLink } from "@taiga-ui/core";
 import { TuiChevron } from "@taiga-ui/kit";
+
+import { FOOTER_SECTIONS } from "../config/footer.config";
 
 @Component({
   standalone: true,
   selector: "app-footer",
-  imports: [TuiActiveZone, TuiButton, TuiChevron, TuiDropdown, TuiObscured],
+  imports: [
+    TuiActiveZone,
+    TuiButton,
+    TuiChevron,
+    TuiDropdown,
+    TuiObscured,
+    TuiLink,
+    RouterLink,
+  ],
   templateUrl: "footer.component.html",
   styleUrl: "footer.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppFooterComponent {}
+export class AppFooterComponent {
+  protected readonly sections = FOOTER_SECTIONS;
+}
