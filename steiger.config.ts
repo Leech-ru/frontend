5fd004df6d2b08edf7d2a25f6f7db8eb4982b85c
Nikita Gaziev <zobweyt@gmail.com>
2025-05-24 22:03:34 +0300

    chore: fix steiger rules for insignificant slice

--- steiger.config.ts
+++ steiger.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from "steiger";
 export default defineConfig([
   ...fsd.configs.recommended,
   {
+    rules: {
+      "fsd/insignificant-slice": "off",
+    },
+  },
+  {
     files: ["./src/shared/**"],
     rules: {
       "fsd/public-api": "off",
