932260bffd453bd89081f28035e6e309ce7c7063
Nikita Gaziev <zobweyt@gmail.com>
2025-06-10 23:28:56 +0300

    fix(leech/buy): make ssr friendly localstorage api

--- src/features/leech/buy/model/storage.ts
+++ src/features/leech/buy/model/storage.ts
@@ -1,22 +1,30 @@
 import { LEECH_BUY_FORM_VALUE_LOCAL_STORAGE_KEY } from "../config";
 
 export const getLeechBuyFormValue = <T extends object>(): T | null => {
-  const item = localStorage.getItem(LEECH_BUY_FORM_VALUE_LOCAL_STORAGE_KEY);
+  try {
+    const item = localStorage.getItem(LEECH_BUY_FORM_VALUE_LOCAL_STORAGE_KEY);
 
-  if (item === null) {
+    if (item === null) {
+      return null;
+    }
+
+    return JSON.parse(item) as T | null;
+  } catch {
     return null;
   }
-
-  return JSON.parse(item) as T | null;
 };
 
 export const saveLeechBuyFormValue = <T extends object>(value: T): void => {
-  localStorage.setItem(
-    LEECH_BUY_FORM_VALUE_LOCAL_STORAGE_KEY,
-    JSON.stringify(value),
-  );
+  try {
+    localStorage.setItem(
+      LEECH_BUY_FORM_VALUE_LOCAL_STORAGE_KEY,
+      JSON.stringify(value),
+    );
+  } catch {}
 };
 
 export const clearLeechBuyFormValue = (): void => {
-  localStorage.removeItem(LEECH_BUY_FORM_VALUE_LOCAL_STORAGE_KEY);
+  try {
+    localStorage.removeItem(LEECH_BUY_FORM_VALUE_LOCAL_STORAGE_KEY);
+  } catch {}
 };
