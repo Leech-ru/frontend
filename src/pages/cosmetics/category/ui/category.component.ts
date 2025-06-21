b99df679d802931162afc776464ff9f8813ef6b3
Ramir Vorobyev <rrramirrr@yandex.ru>
2025-06-21 18:33:00 +0300

    feat(cosmetics/category/:id): add breadcrumbs

--- src/pages/cosmetics/category/ui/category.component.ts
+++ src/pages/cosmetics/category/ui/category.component.ts
@@ -1,15 +1,26 @@
 import { Component, computed, input } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { TuiItem } from "@taiga-ui/cdk";
+import { TuiLink } from "@taiga-ui/core";
+import { TuiBreadcrumbs } from "@taiga-ui/kit";
 
 import { AppCosmeticsItemCardComponent } from "@/entities/cosmetic";
 import { TEST_ITEM_CARDS_STH_CATEGORY } from "@/pages/cosmetics/category/mock";
 import { CosmeticCategories } from "@/shared/config";
+import { AppCalloutComponent } from "@/shared/ui/callout";
 
 @Component({
   selector: "app-category-page",
   styleUrl: "category.component.less",
   templateUrl: "category.component.html",
-  imports: [AppCosmeticsItemCardComponent, RouterLink],
+  imports: [
+    AppCosmeticsItemCardComponent,
+    RouterLink,
+    TuiBreadcrumbs,
+    TuiLink,
+    AppCalloutComponent,
+    TuiItem,
+  ],
 })
 export class AppCosmeticsCategoryPageComponent {
   protected readonly id = input.required<string>();
@@ -17,4 +28,11 @@ export class AppCosmeticsCategoryPageComponent {
     () => CosmeticCategories[Number(this.id())],
   );
   protected readonly items = TEST_ITEM_CARDS_STH_CATEGORY;
+
+  protected breadcrumbs = [
+    {
+      title: "Разделы косметики",
+      routerLink: "/cosmetic/category",
+    },
+  ];
 }
---
+++ src/shared/model/breadcumbs.ts
@@ -0,0 +1,6 @@
+import { RoutePath } from "@/app/@x/route";
+
+export interface BreadCrumb {
+  title: string;
+  routerLink: RoutePath;
+}
