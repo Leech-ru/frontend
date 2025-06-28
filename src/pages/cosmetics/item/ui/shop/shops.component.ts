f4a1d763f637234367805b2ee2fa4b618009a009
Ramir Vorobyev <rrramirrr@yandex.ru>
2025-06-28 16:01:36 +0300

    feat(cosmetics/item/:id): add "Посмотреть еще магазины" to shop-list in item-page

--- src/pages/cosmetics/item/ui/shop/shops.component.ts
+++ src/pages/cosmetics/item/ui/shop/shops.component.ts
@@ -1,4 +1,5 @@
 import { Component, input } from "@angular/core";
+import { RouterLink } from "@angular/router";
 import { TuiButton } from "@taiga-ui/core";
 
 import { CosmeticShops } from "@/entities/cosmetic";
@@ -7,7 +8,7 @@ import { CosmeticShops } from "@/entities/cosmetic";
   selector: "app-cosmetic-item-shops",
   templateUrl: "shops.component.html",
   styleUrl: "shops.component.less",
-  imports: [TuiButton],
+  imports: [TuiButton, RouterLink],
 })
 export class ShopsComponent {
   public data = input<CosmeticShops>({} as CosmeticShops);
