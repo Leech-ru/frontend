603fb47727c8a5a71a8ab5e05077b59079cd6b48
Nikita Gaziev <zobweyt@gmail.com>
2025-06-29 23:20:01 +0300

    feat: add gallery component example usage on about page

--- src/pages/about/ui/page.component.ts
+++ src/pages/about/ui/page.component.ts
@@ -3,14 +3,48 @@ import { TuiLink } from "@taiga-ui/core";
 
 import { DOCUMENT_FOOTER, DOCUMENTS } from "@/pages/about/config";
 import { AppCalloutComponent } from "@/shared/ui/callout";
+import {
+  AppGalleryCarouselItem,
+  AppGalleryComponent,
+} from "@/shared/ui/gallery";
 
 @Component({
-  selector: "app--about-page",
-  imports: [AppCalloutComponent, TuiLink],
+  selector: "app-about-page",
+  imports: [AppCalloutComponent, AppGalleryComponent, TuiLink],
   templateUrl: "page.component.html",
   styleUrl: "page.component.less",
 })
 export class AppAboutPageComponent {
   protected readonly documents = DOCUMENTS;
   protected readonly document_footer = DOCUMENT_FOOTER;
+
+  protected readonly gallery: AppGalleryCarouselItem[] = [
+    [
+      {
+        src: "/assets/about-our-center.jpg",
+      },
+      {
+        src: "/assets/cosmetics-catalog.jpg",
+      },
+    ],
+    {
+      src: "/assets/medical-leech-wholesale.jpg",
+      large: true,
+    },
+    {
+      src: "/assets/about-our-center.jpg",
+    },
+    [
+      {
+        src: "/assets/cosmetics-catalog.jpg",
+      },
+      {
+        src: "/assets/medical-leech-wholesale.jpg",
+      },
+    ],
+    {
+      src: "/assets/about-our-center.jpg",
+      large: true,
+    },
+  ];
 }
