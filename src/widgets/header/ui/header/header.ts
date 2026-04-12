a40141282643b442dba128769f5cead423edab69
Nikita Gaziev <zobweyt@gmail.com>
2026-04-12 15:36:31 +0000

    Fix user dropdown

--- src/widgets/header/ui/header/header.ts
+++ src/widgets/header/ui/header/header.ts
@@ -4,17 +4,31 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
+  OnDestroy,
+  output,
   signal,
 } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { TuiDropdownSheet } from "@taiga-ui/addon-mobile";
-import { TuiObscured } from "@taiga-ui/cdk";
+import { TuiActiveZone, TuiObscured } from "@taiga-ui/cdk";
 import { TuiDataList, TuiDropdown, TuiTitle } from "@taiga-ui/core";
 import { TuiAvatar, TuiBadge, TuiFade, TuiInitialsPipe } from "@taiga-ui/kit";
 import { AppHeaderDrawerComponent } from "../drawer";
 import { AppHeaderLogoComponent } from "../logo";
 import { AppHeaderNavigationComponent } from "../navigation";
 import { AppHeaderNavigationItemComponent } from "../navigation/item";
+
+@Component({
+  selector: "sheet-spy",
+  template: "",
+})
+export class SheetSpy implements OnDestroy {
+  public readonly destroyed = output<void>();
+
+  ngOnDestroy() {
+    this.destroyed.emit();
+  }
+}
 
 @Component({
   selector: "app-header",
@@ -25,7 +39,9 @@ import { AppHeaderNavigationItemComponent } from "../navigation/item";
     AppHeaderLogoComponent,
     AppHeaderNavigationComponent,
     AppHeaderNavigationItemComponent,
+    SheetSpy,
     RouterLink,
+    TuiActiveZone,
     TuiAvatar,
     TuiBadge,
     TuiDataList,
