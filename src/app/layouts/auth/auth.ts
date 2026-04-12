e801a27b2dec76f6de6a04d3e6073e7ce4e9f8f4
Nikita Gaziev <zobweyt@gmail.com>
2026-04-12 16:58:23 +0000

    Improve loading UX for auth

--- src/app/layouts/auth/auth.ts
+++ src/app/layouts/auth/auth.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { isPlatformServer } from "@angular/common";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  PLATFORM_ID,
+} from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
-import { TuiButton } from "@taiga-ui/core";
+import { TuiButton, TuiLoader } from "@taiga-ui/core";
 
 @Component({
-  imports: [RouterOutlet, RouterLink, TuiButton],
+  imports: [RouterOutlet, RouterLink, TuiButton, TuiLoader],
   templateUrl: "auth.html",
   styleUrl: "auth.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppAuthLayoutComponent {}
+export class AppAuthLayoutComponent {
+  protected readonly platform = inject(PLATFORM_ID);
+  protected readonly isServer = computed(() => isPlatformServer(this.platform));
+}
